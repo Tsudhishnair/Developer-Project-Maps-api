8e08700934d3998ef01f1c88c81740fa36c45817
sudhish nair <sudhishanir53@gmail.com>
2019-07-20 16:47:48 +0530

    :sparkles: marker & infowindow

--- maps.js
+++ maps.js
@@ -4,7 +4,24 @@ var map;
 // Functions
 function initFunction() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.7413549, lng: -73.9980244 },
+    center: { lat: 10.7, lng: 76.672675 },
     zoom: 13
+  });
+
+  //variable to set the marker
+  var home = { lat: 10.7, lng: 76.67266 };
+  //marker api
+  var marker = new google.maps.Marker({
+    position: home,
+    map: map,
+    title: "Home"
+  });
+
+  //infowindow
+  var infowindow = new google.maps.InfoWindow({
+    content: "This is my lovely home.Please do visit our lovely home"
+  });
+  marker.addListener("click", function() {
+    infowindow.open(map, marker);
   });
 }
